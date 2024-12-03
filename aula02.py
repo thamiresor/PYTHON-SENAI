286e062d08505953e7590981769964df028aa2af
Thamires <thorodrigues83@gmail.com>
2024-12-03 09:40:48 -0300

    Add files via upload
    Aprendi a criar variáveis e usá-las entre os textos.

---
+++ aula02.py
@@ -0,0 +1,13 @@
+print('Bom dia, agora são 8h53 da manhã, em São Paulo - SP - Brasil')
+print('Ontem eu assisti Moana no cinema com o meu namorado e com a minha cunhada, e ELA QUE QUIS QUE EU FOSSE AAA')
+print('Nesse fim de semana viajei com minha família, tomei MUITO sol e apresentei meu nakmorado pra minha família')
+print('Agora fiquem com a minha avó')
+
+nome_da_vovó = 'Mariene' # tipo -> str = string
+idade_da_vovó = 63 # tipo -> int = integer inteiro
+altura_da_vovó = 1.60 # tipo -> float = ponto flutuante
+minha_avó_é_considerada_nova = True # tipo -> bool = boolean
+
+print('Olá,', nome_da_vovó, 'você está bem?')
+print('Aqui vão algumas informações sobre você. Sua idade é' ,idade_da_vovó ,'anos e você tem' ,altura_da_vovó ,'metros, certo?')
+print('Além disso, as pessoas dizem que você é considerada nova para ser avó?' ,minha_avó_é_considerada_nova)
